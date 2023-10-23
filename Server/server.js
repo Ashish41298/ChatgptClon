b1968d2ce14897e7b46a5aa0ad0cc98b96dbc4ac
Ashish41298 <74136104+Ashish41298@users.noreply.github.com>
2023-10-23 21:09:02 +0530

    hh commit

---
+++ Server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import OpenAI from 'openai';
+import cors from 'cors';
+ import dotenv from 'dotenv';
+ import { AbortController } from 'abort-controller';
+ dotenv.config();
+const app = express();
+app.use(cors());
+const port = process.env.PORT | 3000;
+app.use(bodyParser.json());
+app.get('/api/', async (req, res) =>{
+  res.send("🪟⚛️🕉️ now bot started");
+});
+  
+app.post('/api/chat', async (req, res) => {
+  const prompt = await req.body.prompt;
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPEN_AI_KEY,
+    });
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: prompt }],
+        model: 'gpt-3.5-turbo',
+      });
+    res.status(200).send({
+      bot : chatCompletion.choices[0].message
+                })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'An error occurred' });
+  }
+ });
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
